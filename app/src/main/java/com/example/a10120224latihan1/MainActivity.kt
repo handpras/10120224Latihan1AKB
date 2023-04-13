88328e675f5afff407c0238ead351da66305492c
handpras <hanung.prasetyo19@gmail.com>
2023-04-13 08:25:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10120224latihan1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a10120224latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// NIM : 10120224
+// Nama : Hanung Dwi Prasetyo
+// Kelas : IF-6
+// Tanggal pengerjaan : 12 April 2023
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
